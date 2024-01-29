e392726343608abc40aa8060699a81b31e36f9e1
karldreta <karldreta@gmail.com>
2024-01-29 17:24:44 +0800

    Finish the project

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ Odin JS Project #1 - Rock, Paper, Scissors
 
 MY task is to make a simple Rock, Paper, Scissors game.
 
-I said simple, but I may not be the case, regardless.
+I said simple, but it may not be the case, regardless.
 
 ## Happy Coding!
+
+## Aftermath
+
+Finally, it was so hard.
+
+I have commented my learnings in the script file but hears some of it:
+
+--- I learned scoping variables
+--- Learned a lot about conditionals 
+--- Had to advance the lessons to get into loops here: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code.
+
+I know I'm not supposed to step into rabbit holes, I'll just go back.
+
+Anyways, I think I finished the project, hopefully.
--- script.js
+++ script.js
@@ -1,48 +1,100 @@
-let x = "Rock";
-let y = "Paper";
-let z = "Scissors";
-const choices = [x, y, z]
+// FIRST JS PROJECT by Karl D Reta of the Odin Project
+
+// Declare constant variables
+// choices is the array of options for the computer
+// x and y will be used later to give a number value to for the result of each round
+
+const choices = ["Rock", "Paper", "Scissors"];
+const x = "YOU WIN, ";
+const y = "YOU LOSE, ";
 
 function getComputerChoice () {
+    // creates a variable randomIndex equalizes it to a random decimal value (0 - 1), 
+    // multiplies it to the length (0-2 in this case) of the choices array
+    // and "floors it down" to the nearest whole number.
+    // then, returns choices array with the randomly chosen number, that number is the index position of any string in the array.
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-let computerSelection = getComputerChoice();
-let anyCase = prompt("Choose: Rock, Paper or Scissors.").toLowerCase();
-let playerSelection = anyCase;
 
-// Begin Round -->
 
-function playRound(playerSelection,computerSelection) {
+function playRound() {
+    // The first three lines should be nested inside of the function (never outside), for the prompt to called multiple times.
+    // variable computerSelection takes the value from the global getComputerChoice function.
+    // anyCase asks for prompt and converts it to lowercase.
+    // playerSelection now holds the value of anyCase.
+    // The conditions strictly uses lowercase for playerSelection, since playerSelection is in lowerCase after all.
 
-   if (playerSelection === "rock" && computerSelection === y) {
-    return "LOSE. Paper covers rock.";
-   } else if (playerSelection === "rock" && computerSelection === z) {
-    return "WIN! Rock beats scissors.";
-   } else if (playerSelection === "paper" && computerSelection === x){
-    return "WIN! Paper covers rock.";
-   } else if (playerSelection === "paper" && computerSelection === z) {
-    return "LOSE. Scissors cuts paper.";
-   }else if (playerSelection === "scissors" && computerSelection === x) {
-    return "LOSE. Rock crushes scissors."; 
-   } else if (playerSelection === "scissors" && computerSelection === y) {
-    return "WIN! Scissors cuts paper.";
-   } else if (!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors")) {
-    return "Choose only: Rock, Paper or Scissors, try again.";
-} else {
-    return "A stalemate.";
-   }
+    let computerSelection = getComputerChoice();
+    let anyCase = prompt("Choose: Rock, Paper or Scissors.").toLowerCase();
+    let playerSelection = anyCase;
+
+    if (playerSelection === "rock" && computerSelection === "Paper") {
+        return y + "paper covers rock.";
+    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        return x + "rock beats scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "Rock"){
+        return x + "paper covers rock.";
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        return y + "scissors cuts paper.";
+    }else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        return y + "rock crushes scissors."; 
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return x + "scissors cuts paper.";
+    } else {
+        return "A stalemate."; 
+    }
 }
 
-// Call the rounds 
-// Set a best of five game
+function roundResult() {
+    // This function should return a value for each round
+    // Creates a local variable "result", takes the value from playRound function.
+    // if the return value of the result variable has either the x or y variable (declared globally), returns 1 and 0 respectively.
+    // Otherwise, returns null.
+
+    let result = playRound();
+    console.log(result);
+    if (!(result.includes(x) || result.includes(y))) {
+        return null
+    } else if (result.includes(x)) {
+        return 1;
+    } else {
+        return 0
+    } 
+}
+
+// Begin best of five Game
 
 function game() {
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection)); 
+    // Initialize the scores, "The Score Board"
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // Used a for loop here, 
+    // Created variable "round", initialized it to 1.
+    // As long as round <= 5; round gets incremented.
+    // Created another local variable "result", takes the value from the roundResult function.
+    // if-else used to get the result of each round and add it to the "Score Board."
+    // Another if-else outside of the for loop, which takes the values from "The Score Board", and compares to display a sixth and last log.
+    for (let round = 1; round <= 5; round++) {
+        let result = roundResult();
+
+        if (result === 1) {
+            playerScore++;
+        } else if (result === 0) {
+            computerScore++;
+        }
+    }
+
+        if (playerScore > computerScore) {
+            console.log("Congratulations, you beat the computer!");
+        }else if (playerScore < computerScore) {
+            console.log("Sorry, better luck next time.");
+        } else {
+            console.log("An impasse.");
+        }
 }
+
+// Finally call the Function
 
 game();
